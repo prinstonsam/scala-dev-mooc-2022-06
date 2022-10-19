daec6634a45aff7444fe5a5bdb37702e0f4c4d0f
Igor Samsonov <isamsonov@apple.com>
2022-10-19 16:07:05 +0200

    Futures homework

--- src/main/scala/futures/task_futures_sequence.scala
+++ src/main/scala/futures/task_futures_sequence.scala
@@ -1,8 +1,11 @@
 package futures
 
 import HomeworksUtils.TaskSyntax
+import zio.CancelableFuture
 
+import scala.concurrent.impl.Promise
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object task_futures_sequence {
 
@@ -15,11 +18,39 @@ object task_futures_sequence {
    * в правово результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
+
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
+
+  def transform[A](l: List[Either[Throwable, A]]): (List[A], List[Throwable]) = {
+    l.foldLeft(List.empty[A], List.empty[Throwable])((a, b) => {
+      b match {
+        case Right(value) => (a._1 :+ value, a._2)
+        case Left(error) => (a._1, a._2 :+ error)
+      }
+    })
+  }
+
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`"()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+
+    val listFutures = futures.map {
+      item =>
+        item
+          .map(Right(_))
+          .recover {
+            case error: Throwable =>
+              Left(error)
+          }
+    }
+
+
+    val result = Future.sequence(listFutures).map {
+      item => transform(item)
+    }
+
+    result
+  }
 }
